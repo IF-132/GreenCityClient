24dcab87f5efce32a01ba2dc1345cc457584d948
IreneKatrych <irenecherokee@gmail.com>
2021-11-25 02:05:32 +0200

    employee form linter errors fixed

--- src/app/ubs-admin/components/ubs-admin-employee/employee-form/employee-form.component.spec.ts
+++ src/app/ubs-admin/components/ubs-admin-employee/employee-form/employee-form.component.spec.ts
@@ -38,137 +38,137 @@ describe('EmployeeFormComponent', () => {
   });
 
   it('First name field should be invalid when empty', () => {
-    let firstName = component.employeeForm.controls['firstName'];
+    const firstName = component.employeeForm.controls.firstName;
     expect(firstName.valid).toBeFalsy();
   });
 
   it('First name field should be invalid when user input numbers', () => {
-    let firstName = component.employeeForm.controls['firstName'];
+    const firstName = component.employeeForm.controls.firstName;
     let errors = {};
     firstName.setValue('345');
     errors = firstName.errors || {};
-    expect(errors['pattern']).toBeTruthy();
+    expect(Object.keys(errors)).toContain('pattern');
   });
 
   it('First name field should be invalid when user input punctuation marks like &?!', () => {
-    let firstName = component.employeeForm.controls['firstName'];
+    const firstName = component.employeeForm.controls.firstName;
     let errors = {};
     firstName.setValue('$&?!');
     errors = firstName.errors || {};
-    expect(errors['pattern']).toBeTruthy();
+    expect(Object.keys(errors)).toContain('pattern');
   });
 
   it('First name field should be valid when user input letters', () => {
-    let firstName = component.employeeForm.controls['firstName'];
+    const firstName = component.employeeForm.controls.firstName;
     firstName.setValue('test');
     expect(firstName.valid).toBeTruthy();
   });
 
   it('First name field should be valid when user input ukrainian letters', () => {
-    let firstName = component.employeeForm.controls['firstName'];
+    const firstName = component.employeeForm.controls.firstName;
     firstName.setValue('Тест');
     expect(firstName.valid).toBeTruthy();
   });
 
   it('First name field should be valid when user input name on other languages (korean for example)', () => {
-    let firstName = component.employeeForm.controls['firstName'];
+    const firstName = component.employeeForm.controls.firstName;
     firstName.setValue('하');
     expect(firstName.valid).toBeTruthy();
   });
 
   it('First name field should be valid when user input letters, spaces, dashes and apostrophe', () => {
-    let firstName = component.employeeForm.controls['firstName'];
+    const firstName = component.employeeForm.controls.firstName;
     firstName.setValue("Conan'O-Brian Lionel");
     expect(firstName.valid).toBeTruthy();
   });
 
   it('Last name field should be invalid when empty', () => {
-    let lastName = component.employeeForm.controls['lastName'];
+    const lastName = component.employeeForm.controls.lastName;
     expect(lastName.valid).toBeFalsy();
   });
 
   it('Last name field should be invalid when user input numbers', () => {
-    let lastName = component.employeeForm.controls['lastName'];
+    const lastName = component.employeeForm.controls.lastName;
     let errors = {};
     lastName.setValue('678');
     errors = lastName.errors || {};
-    expect(errors['pattern']).toBeTruthy();
+    expect(Object.keys(errors)).toContain('pattern');
   });
 
   it('Last name field should be invalid when user input punctuation marks like &?!', () => {
-    let lastName = component.employeeForm.controls['lastName'];
+    const lastName = component.employeeForm.controls.lastName;
     let errors = {};
     lastName.setValue('$&?!');
     errors = lastName.errors || {};
-    expect(errors['pattern']).toBeTruthy();
+    expect(Object.keys(errors)).toContain('pattern');
   });
 
   it('Last name field should be valid when user input letters', () => {
-    let lastName = component.employeeForm.controls['lastName'];
+    const lastName = component.employeeForm.controls.lastName;
     lastName.setValue('test');
     expect(lastName.valid).toBeTruthy();
   });
 
   it('Last name field should be valid when user input ukrainian letters', () => {
-    let lastName = component.employeeForm.controls['lastName'];
+    const lastName = component.employeeForm.controls.lastName;
     lastName.setValue('Тест');
     expect(lastName.valid).toBeTruthy();
   });
 
   it('Last name field should be valid when user input name on other languages (korean for example)', () => {
-    let lastName = component.employeeForm.controls['lastName'];
+    const lastName = component.employeeForm.controls.lastName;
     lastName.setValue('현상');
     expect(lastName.valid).toBeTruthy();
   });
 
   it('Last name field should be valid when user input letters, spaces, dashes and apostrophe', () => {
-    let lastName = component.employeeForm.controls['lastName'];
+    const lastName = component.employeeForm.controls.lastName;
     lastName.setValue("Mortimer'D Chester-great-son");
     expect(lastName.valid).toBeTruthy();
   });
 
   it('Phone number field should be invalid when empty', () => {
-    let phoneNumber = component.employeeForm.controls['phoneNumber'];
+    const phoneNumber = component.employeeForm.controls.phoneNumber;
     expect(phoneNumber.valid).toBeFalsy();
   });
 
   it('Phone number field should be valid with normal number', () => {
-    let phoneNumber = component.employeeForm.controls['phoneNumber'];
+    const phoneNumber = component.employeeForm.controls.phoneNumber;
     phoneNumber.setValue('+380111111111');
     expect(phoneNumber.valid).toBeTruthy();
   });
 
   it('Form is invalid when phone number field value is too short', () => {
-    let phoneNumber = component.employeeForm.controls['phoneNumber'];
+    const phoneNumber = component.employeeForm.controls.phoneNumber;
     phoneNumber.setValue('+3801111');
     expect(component.employeeForm.invalid).toBeTruthy();
   });
 
   it('Email field should be invalid when empty', () => {
-    let email = component.employeeForm.controls['email'];
+    const email = component.employeeForm.controls.email;
     expect(email.valid).toBeFalsy();
   });
 
   it('Email field should be invalid without @ in the middle', () => {
-    let email = component.employeeForm.controls['email'];
+    const email = component.employeeForm.controls.email;
     email.setValue('test');
     expect(email.invalid).toBeTruthy();
   });
 
   it('Email field should be valid with normal mail', () => {
-    let email = component.employeeForm.controls['email'];
+    const email = component.employeeForm.controls.email;
     email.setValue('test@mail.com');
     expect(email.valid).toBeTruthy();
   });
 
   it('Form should be valid with all required fields filled (except employee position and receiving station)', () => {
-    let firstName = component.employeeForm.controls['firstName'];
+    const firstName = component.employeeForm.controls.firstName;
     firstName.setValue('test');
-    let lastName = component.employeeForm.controls['lastName'];
+    const lastName = component.employeeForm.controls.lastName;
     lastName.setValue('test');
-    let phoneNumber = component.employeeForm.controls['phoneNumber'];
+    const phoneNumber = component.employeeForm.controls.phoneNumber;
     phoneNumber.setValue('+380111111111');
-    let email = component.employeeForm.controls['email'];
+    const email = component.employeeForm.controls.email;
     email.setValue('test@mail.com');
     expect(component.employeeForm.valid).toBeTruthy();
   });
